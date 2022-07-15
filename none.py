4915230508826ed0d5268d2b7932c7c5e6dd7eeb
ingaroca26 <90444094+ingaroca26@users.noreply.github.com>
2022-07-14 19:19:05 -0500

    Create none.py

---
+++ none.py
@@ -0,0 +1,14 @@
+data: None = None
+print(data)
+# None
+
+
+information: None = type(None)()
+print(information)
+# None
+
+
+details: None = None
+details_type = type(details)
+print(details_type)
+# <class 'NoneType'>
